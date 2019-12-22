f8c3c4e358c7f4139658d91c15ae18e8f7200a25
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-22 15:06:37 +0000

    Deploy jellyb0y/2019-2-Atom-Frontend-V-Kisel to github.com/jellyb0y/2019-2-Atom-Frontend-V-Kisel.git:gh-pages

---
+++ precache-manifest.77591a910e7fb798779f360dd93e49b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09a4b222e742073886632252194b374",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/index.html"
+  },
+  {
+    "revision": "99a47af6003022e448b7",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/css/main.52e8ecdb.chunk.css"
+  },
+  {
+    "revision": "e0ac303dcbfd4b3e298f",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/js/2.ac5235dd.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/js/2.ac5235dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99a47af6003022e448b7",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/js/main.d5545802.chunk.js"
+  },
+  {
+    "revision": "7cb7faafd4d98e7808cc",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/js/runtime-main.e1283f66.js"
+  },
+  {
+    "revision": "32d9c803755d27322addd016642c6a28",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/media/AgoraSansProBold.32d9c803.eot"
+  },
+  {
+    "revision": "3c45a7faadf49e2fb5ef6b9150ed5e86",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/media/AgoraSansProBold.3c45a7fa.woff"
+  },
+  {
+    "revision": "db7576c2bea2b20e5e68e19d35c64f1e",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/media/AgoraSansProBold.db7576c2.ttf"
+  },
+  {
+    "revision": "54c5eab034b6a2ce22742d6898309014",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/media/VoiceoftheHighlander.54c5eab0.ttf"
+  },
+  {
+    "revision": "676c0051b425754336d413c4bf57005e",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/media/VoiceoftheHighlander.676c0051.eot"
+  },
+  {
+    "revision": "f6e5ffd2e811764a96cf331a77d9d3d2",
+    "url": "/2019-2-Atom-Frontend-V-Kisel/static/media/VoiceoftheHighlander.f6e5ffd2.woff"
+  }
+]);
